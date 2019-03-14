83a2eb29d36d298b95b52f1f2b25b266e56d96be
joshuamurphy <joshuamurphy@mitre.org>
2019-03-14 16:24:28 -0400

    removed keys from repo

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     google: {
         clientID: "1084032731156-650j7fsjup377gl2rglfb6a6ahcp1q1a.apps.googleusercontent.com",
@@ -8,7 +7,7 @@ module.exports = {
         dbURI: "mongodb+srv://I2-user:abc123@i2-oauth-wxesw.mongodb.net/test?retryWrites=true"
     },
     openID: {
-        clientID: "7b07274b-3875-426c-8a8a-61f2b4e12583",
-        clientSecret: "KM2lSnNkC-k8W2JUJmFwdsIGnMpLB1nU-AFLDhubV1p6KYlJhoRPYp7uwlHxmt4cOnaah7qWL6coAM_kJHhh9A"
+        clientID: "7b5d9dd8-4013-4996-a104-cbf112793c2e",
+        clientSecret: "PT4gGu-L3xxr6z7ZS5OLOgcjZ7ZTwo7J8uk1-o64nLNVdEqgjZvEcVY6I51__wkYnQniHD7ln04Vtg-bYd87GQ"
     }
 }
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,7 +2,6 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const OpenIdStrategy = require('passport-openidconnect').Strategy;
 const keys = require('./keys');
-const User = require('../models/user-model');
 
 // passport.serializeUser((user, done));
 
