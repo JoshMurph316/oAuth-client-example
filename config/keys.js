6e72af43355274c552ccff5905cc03b6a5c8b05a
joshuamurphy <joshuamurphy@mitre.org>
2019-03-27 14:05:02 -0400

    added key file

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    google: {
+        clientID: "YOUR_KEY_HERE",
+        clientSecret: "YOUR_SECRET_HERE"
+    },
+    openID: {
+        clientID: "YOUR_KEY_HERE",
+        clientSecret: "YOUR_SECRET_HERE"
+    }
+}
